009e2098982e25beec6faaadcee022e944cd1a48
2938137849 <2938137849@qq.com>
2021-01-01 16:08:47 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# nodejsbot
- nodejs
+# NodeJsBot
+
+## 1. 背景
+
+使用 `nodejs` 作为开发/运行环境,一个支持热更新,热重载的qq机器人实现.
+
+使用 `正向websocket` 连接 [`go-cqhttp`(点击前往)](https://github.com/Mrs4s/go-cqhttp) 开启的qq消息服务
+
+目标是频繁更新功能均使用插件实现.让其支持热更新与热重载
+
+## 2. 横幅
+
+<details><summary>老DD了</summary>
+
+![老DD了](./400x400.png)
+
+</details>
+
+## 3. 目录
+
+- [背景](#1-)
+- [横幅](#2-)
+- [目录](#3-)
+- [安装](#4-)
+- [API](#5-api)
+
+## 4. 安装
+
+本项目使用的 [`go-cqhttp`(点击前往)](https://github.com/Mrs4s/go-cqhttp) 项目请自行下载并配置运行
+
+1. 首先安装 [`nodejs`(点击前往)](http://nodejs.cn/download/) 运行环境
+2. 克隆本项目
+3. 进入项目目录,执行 `npm i` 下载依赖
+4. 在项目目录中新建文件夹 `config`
+5. 在 `config` 文件夹中新建文件 `config.json` 并写入如下配置
+
+   **注1**: 写入文件时请删除注释,防止出错
+
+   **注2**: 本配置文件为插件 `bot.js` 中需求,建议自行修改后硬编码进js文件
+
+    ```json5
+    {
+      // cqws 部分参数具体参考 node库: 
+      // 'cq-websocket' 中 'CQWebSocketOption'
+      "cqws": {
+        "accessToken": "",
+        "enableAPI": true,
+        "enableEvent": true,
+        "protocol": "ws:",
+        "host": "",
+        "port": 0,
+        // baseUrl 参数与 (host / port) 参数中二选一
+        "baseUrl": "",
+        "qq": 0,
+        "reconnection": true,
+        "reconnectionAttempts": 10,
+        "reconnectionDelay": 5000
+      },
+      // adminID 为管理员QQ号码,
+      "adminId": 0
+    }
+    ```
+
+6. 回到项目根目录,执行 `npm run run`
+7. bot启用
+
+## 5. API
+
+参考项目根目录中 [`Plugin.js`(点击查看)](./Plugin.js) 文件,所有插件均继承自本类
+
+插件加载器: [`PluginLoader.js`(点击查看)](./PluginLoader.js) 所有插件均被此插件加载器加载,均加载自 `./plugin/` 目录下
+
+简易插件写法: [`test.js`(点击查看)](./plugin/test.js)
+
--- test.js
+++ test.js
@@ -1,20 +1,136 @@
-// const Plugin = require("./Plugin");
-const PluginLoader = require("./PluginLoader");
-const utils = require("./src/utils");
+const {cqws, adminId} = require("./config/config.json");
+const utils = require('./src/utils');
+const CQ = require("./src/CQ")
+const Repeat = require("./src/repeat");
+const NAO = require("./src/SauceNAOUtil");
 
-/**
- *
- * @type {PluginLoader}
- */
-let loader = new PluginLoader();
+function admin(message) {
+  return {
+    user_id: adminId,
+    message: message
+  }
+}
+
+function success(ret) {
+  console.log(`发送成功`, ret.data);
+}
+
+function fail(reason) {
+  console.log(`发送失败`, reason);
+}
 
 
-loader.toggle(null, loader).then(() => {
-  console.log(">>>>>>>>>> 启动 <<<<<<<<<<")
-}).then(() => {
-  return loader.refreshPlugin();
-}).then(() => {
-  console.log(`${utils.now()} `, loader.plugins);
-  return loader.handle(true, "HttpOption", /*"CQBot"*/);
+const bot = utils.openCQWebSocket(cqws);
+const repeat = new Repeat();
+
+
+bot.on("message.private", (event, context, tags) => {
+  console.log(event);
+  console.log(context);
+  console.log(tags);
+
+  console.log("收到消息", context, tags);
+  for (let tag of tags) {
+    if (tag.tagName === "image") {
+      let url = tag.url;
+      if (url) {
+        console.log("开始搜图");
+        NAO.search(url, {
+          testmode: 1,
+          db: 5,
+          numres: 1,
+        }).then(result => {
+          if (result.hasResult) {
+            let first = result.results[0];
+            console.log("有结果", first);
+            bot.send(
+                "send_private_msg",
+                admin([
+                  CQ.image(first.thumbnail),
+                  CQ.text(`相似度: ${first.similarity}%\n`),
+                  CQ.text(`具体信息已舍弃`)
+                  //TODO:作者&图片源
+                ])
+            ).then(success, fail)
+          } else {
+            console.log("搜图无结果");
+            bot.send("send_private_msg", admin([
+                  CQ.text(`搜图无结果`)
+                ])
+            ).then(success, fail)
+          }
+        })
+      }
+      // stopPropagation方法阻止事件冒泡到父元素
+      event.stopPropagation()
+      break;
+    }
+  }
+
 });
 
+bot.on("message.group.@.me", (event, context, tags) => {
+  let groupId = context.group_id;
+  let messageId = context.message_id;
+  let userId = context.sender.user_id;
+  /**
+   * @type {[]}
+   */
+  let cqTags = tags.filter(tag => tag.tagName === "text");
+
+  // stopPropagation方法阻止事件冒泡到父元素
+  event.stopPropagation()
+
+  bot.send("send_group_msg", {
+    group_id: groupId,
+    message: [
+      CQ.reply(messageId),
+      CQ.at(userId),
+      ...cqTags
+    ]
+  }).then(success, fail)
+})
+
+bot.on("message.group", (event, context, tags) => {
+  let groupId = context.group_id;
+  // let messageId = context.message_id;
+  let userId = context.sender.user_id;
+
+  if (tags.length === 1 && tags[0].tagName === "text") {
+    let msg = tags[0].text;
+    if (msg.startsWith("-")) {
+      // TODO: 命令行指令
+    } else {
+      if (repeat.check(groupId, userId, msg, 3)) {
+        setTimeout(() => {
+          bot.send("send_group_msg", {
+            group_id: groupId,
+            message: [
+              tags[0]
+            ]
+          }).then(success, fail)
+        }, 1000)
+      }
+    }
+  }
+})
+
+
+//////////////////////////////////////////////////////////
+// 开始
+//////////////////////////////////////////////////////////
+bot.connect();
+utils.httpStop(() => {
+  return bot.send('send_private_msg', admin("即将下线")).then(ret => {
+    console.log("发送成功", ret.data);
+    bot.disconnect();
+  }).catch(() => {
+    console.log("发送超时");
+    bot.disconnect();
+  });
+})
+
+
+module.exports = {
+  bot
+};
