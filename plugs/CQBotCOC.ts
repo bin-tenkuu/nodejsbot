cda40bf0dc9e1fa50693fec051e9f3a64dc86f8d
2938137849 <2938137849@qq.com>
2021-09-09 15:19:02 +0800

    同步

--- plugs/CQBotCOC.ts
+++ plugs/CQBotCOC.ts
@@ -10,6 +10,7 @@ class CQBotCOC extends Plug {
 	private shortKey = new Map<string, string>();
 	private cheater: boolean;
 	private cache: DataCache<DiceCache>;
+	private specialEffects: string;
 
 	constructor() {
 		super(module);
@@ -18,7 +19,7 @@ class CQBotCOC extends Plug {
 		this.version = 1;
 		this.cheater = false;
 		this.cache = new DataCache<DiceCache>({useClones: false, stdTTL: 60 * 5, deleteOnExpire: true});
-
+		this.specialEffects = "bug";
 		this.readShortKey();
 	}
 
@@ -79,7 +80,7 @@ class CQBotCOC extends Plug {
 		if (/[^+\-*dD0-9#]/.test(dice)) {
 			return [CQ.text(".d错误参数")];
 		}
-		let result = Array.from({length: +times}, () => this.dice(dice, event.context.user_id)).join("\n")
+		let result = Array.from({length: +times}, () => this.dice(dice, event.context.user_id)).join("\n");
 		return [CQ.text(result)];
 	}
 
@@ -98,10 +99,30 @@ class CQBotCOC extends Plug {
 
 	@canCallGroup()
 	@canCallPrivate()
-	async setCheater(event: CQEvent<"message.group"> | CQEvent<"message.private">) {
+	async setCheater(event: CQEvent<"message.group"> | CQEvent<"message.private">): Promise<CQTag[]> {
 		event.stopPropagation();
 		this.cheater = !this.cheater;
 		return [CQ.text("全1" + (this.cheater ? "开" : "关"))];
+	}
+
+	@canCallGroup()
+	@canCallPrivate()
+	async setDiceType(event: CQEvent<"message.group"> | CQEvent<"message.private">,
+			execArray: RegExpExecArray): Promise<CQTag[]> {
+		event.stopPropagation();
+		let {operator} = execArray.groups as { operator: string } ?? {};
+		if (operator == null) {
+			return [];
+		}
+		if (operator === "bug") {
+			this.specialEffects = "bug";
+			return [CQ.text("进入默认状态")];
+		}
+		if (SpecialEffects.has(operator)) {
+			this.specialEffects = operator;
+			return [CQ.text(`进入${SpecialEffects.getState(operator)}状态`)];
+		}
+		return [CQ.text("未知状态")];
 	}
 
 	@canCallGroup()
@@ -137,27 +158,28 @@ class CQBotCOC extends Plug {
 	}
 
 	private dice(str: string, userId: number): string {
-		let handles = str.split(/(?=[+\-*])/).map<calc>(value => {
+		let handles = str.split(/(?=[+\-*])/).map<Calc>(value => {
 			return CQBotCOC.castString(value, this.cheater);
 		});
+		if (handles.length === 1) {
+			let calc: Calc = handles[0];
+			if (calc.list !== null) {
+				this.cache.set(userId, new DiceCache(calc.max, calc!.list));
+				SpecialEffects.runFunc(this.specialEffects, calc);
+				return `${calc.origin}：[${calc.list}]=${calc.num}${calc.state ?? ""}`;
+			} else {
+				return `${calc.op}${calc.origin}=${calc.num}`;
+			}
+		}
 		let preRet = handles.filter(v => v.list !== null).map((v) => {
 			return `${v.origin}：[${v.list}]=${v.num}`;
 		}).join("\n");
 		str = handles.map(value => `${value.op}${value.origin}`).join("");
 		let sumNum = CQBotCOC.calculate(handles);
-		if (preRet !== "") {
-			if (handles.length === 1) {
-				this.cache.set(userId, new DiceCache(handles[0].max, handles[0].list ?? <number[]>[]));
-				return preRet;
-			} else {
-				return `${preRet}\n${str}=${sumNum}`;
-			}
-		} else {
-			return `${str}=${sumNum}`;
-		}
+		return `${preRet}\n${str}=${sumNum}`;
 	}
 
-	private static castString(value: string, cheater: boolean): calc {
+	private static castString(value: string, cheater: boolean): Calc {
 		let groups = /^(?<op>[+\-*])?(?<num>\d+)?(?:[dD](?<max>\d+))?$/.exec(value)?.groups as {
 			op?: "+" | "-" | "*"
 			num?: string
@@ -193,7 +215,7 @@ class CQBotCOC extends Plug {
 		}
 	}
 
-	private static calculate(handles: calc[]): number {
+	private static calculate(handles: Calc[]): number {
 		const map = {
 			"*": (sum: [number, number], num: number) => {
 				sum[1] *= num;
@@ -223,7 +245,8 @@ class CQBotCOC extends Plug {
 	}
 }
 
-type calc = { op: "+" | "-" | "*", num: number, list: Uint16Array | null, origin: string, max: number }
+type EffectFunc = (data: Calc) => void;
+type Calc = { op: "+" | "-" | "*", num: number, list: Uint16Array | null, origin: string, max: number, state?: string };
 
 class DiceCache extends Equatable {
 	public max: number;
@@ -239,6 +262,95 @@ class DiceCache extends Equatable {
 		return obj instanceof DiceCache && obj.max === this.max;
 	}
 
+}
+
+class SpecialEffects {
+	private static Effects: { readonly [key: string]: [string, EffectFunc] | undefined } = Object.freeze({
+		"bug": [
+			"默认", _ => void 0,
+		],
+		"wrf": [
+			"温柔f", data => {
+				let list: Uint16Array | null = data.list;
+				if (list === null) {
+					return;
+				}
+				if (list.length > 2 && list[0] === list[1]) {
+					++list[1];
+				}
+				data.state = "[温柔]";
+			},
+		],
+		"cbf": [
+			"残暴f", data => {
+				let list: Uint16Array | null = data.list;
+				if (list === null) {
+					return;
+				}
+				list[1] = list[0];
+				data.state = "[残暴]";
+			},
+		],
+		"ajf": [
+			"傲娇f", data => {
+				if (Math.random() < 0.5) {
+					return SpecialEffects.getFunc("wrf")(data);
+				} else {
+					return SpecialEffects.getFunc("cbf")(data);
+				}
+			},
+		],
+		"wr": [
+			"温柔", data => {
+				if (Math.random() < 0.5) {
+					return SpecialEffects.getFunc("wrf")(data);
+				} else {
+					return SpecialEffects.getFunc("bug")(data);
+				}
+			},
+		],
+		"cb": [
+			"残暴", data => {
+				if (Math.random() < 0.5) {
+					return SpecialEffects.getFunc("cbf")(data);
+				} else {
+					return SpecialEffects.getFunc("bug")(data);
+				}
+			},
+		],
+		"aj": [
+			"傲娇", data => {
+				let random = Math.random() * 3;
+				if (random < 1) {
+					return SpecialEffects.getFunc("wrf")(data);
+				}
+				if (random < 2) {
+					return SpecialEffects.getFunc("cbf")(data);
+				}
+				return SpecialEffects.getFunc("bug")(data);
+			},
+		],
+	});
+
+	private static getFunc(key: string): EffectFunc {
+		return (this.Effects[key] ?? this.Effects["bug"])![1];
+	}
+
+	public static runFunc(key: string, calc: Calc): void {
+		return this.getFunc(key)(calc);
+	}
+
+	public static getState(key: string): string {
+		let effect = this.Effects[key];
+		if (effect === undefined) {
+			return "默认";
+		}
+		return effect[0];
+	}
+
+	public static has(key: string): boolean {
+		return Reflect.has(this.Effects, key);
+	}
 }
 
 export default new CQBotCOC();
