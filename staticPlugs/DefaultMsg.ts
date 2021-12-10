16b2ac396b1f624599e71310c82cce96e0d172a2
2938137849 <2938137849@qq.com>
2021-12-10 09:49:59 +0800

    同步

--- staticPlugs/DefaultMsg.ts
+++ staticPlugs/DefaultMsg.ts
@@ -1,7 +1,6 @@
 import {Plug} from "../Plug.js";
-import {canCall} from "@U/Corpus.js";
-import {isAdmin, sendAdminQQ} from "@U/Util.js";
-import {Corpus, CorpusData} from "@U/Corpus.js";
+import {canCall, Corpus, CorpusData} from "@U/Corpus.js";
+import {sendAdminQQ} from "@U/Util.js";
 
 export class DefaultMsg extends Plug {
 	@canCall({
@@ -44,31 +43,35 @@ export class DefaultMsg extends Plug {
 		deleteMSG: 10,
 	})
 	protected sendReport({event, execArray}: CorpusData): string {
-		const {txt}: { txt?: string } = execArray.groups as { txt?: string } ?? {};
 		event.stopPropagation();
+		const {txt}: { txt?: string } = execArray.groups as { txt?: string } ?? {};
 		const {nickname, user_id} = event.context.sender;
 		sendAdminQQ(event.bot, `来自 ${nickname} (${user_id}):\n${txt}`).catch(global.NOP);
 		return "收到";
 	}
 
 	@canCall({
-		name: ".(help|帮助)<id>",
-		regexp: /^[.．。](?:help|帮助)(?<num> *\d*)$/,
+		name: ".(help|帮助)[<id>]",
+		regexp: /^[.．。](?:help|帮助)(?<num> ?\d+)?$/,
 		forward: true,
 		weight: 2,
 		minLength: 3,
 		maxLength: 10,
+		deleteMSG: 30,
 	})
 	protected getHelp({event, execArray}: CorpusData): string {
+		event.stopPropagation();
 		const {num} = execArray.groups as { num: string } ?? {};
-		const predicate: (c: Corpus) => boolean = isAdmin(event) ?
-				c => c.help != null :
-				c => c.isOpen > 0 && !c.needAdmin && c.help != null;
-		const corpuses: Corpus[] = Plug.corpuses.filter(predicate);
+		const corpuses: Corpus[] = Plug.corpuses.filter(c => c.help != null);
 		if (+num > 0) {
 			const corpus: Corpus | undefined = corpuses[+num];
 			if (corpus != null) {
-				return `${corpus.name}${corpus.help}`;
+				const {name, help, regexp, minLength, maxLength, speedLimit, deleteMSG} = corpus;
+				return `名称：${name}\n帮助：${help}\n匹配：${regexp}\n长度限制：${minLength}<=%s<=${maxLength}\n${
+						deleteMSG > 0 ? `撤回延时：${deleteMSG}毫秒\n` : ""
+				}${
+						speedLimit > 0 ? `速度限制：1次每${speedLimit}毫秒` : ""
+				}`;
 			}
 		}
 		return corpuses.map<string>((c, i) => `${i} :${c.name}`).join("\n");
